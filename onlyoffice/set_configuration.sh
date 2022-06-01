a35906ea9b38764de88bc4ad48dbdaffdc0e7b11
Agathys <75174461+Agathys@users.noreply.github.com>
2022-06-02 00:03:54 +0300

    Add files via upload
    Файлы для развертывания системы

---
+++ onlyoffice/set_configuration.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+
+docker exec -u www-data app-server php occ --no-warnings config:system:get trusted_domains >> trusted_domain.tmp
+
+if ! grep -q "nginx-server" trusted_domain.tmp; then
+    TRUSTED_INDEX=$(cat trusted_domain.tmp | wc -l);
+    docker exec -u www-data app-server php occ --no-warnings config:system:set trusted_domains $TRUSTED_INDEX --value="nginx-server"
+fi
+
+rm trusted_domain.tmp
+
+docker exec -u www-data app-server php occ --no-warnings app:install onlyoffice
+
+docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/ds-vpath/"
+docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://onlyoffice-document-server/"
+docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://nginx-server/"
